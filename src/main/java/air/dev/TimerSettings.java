9d661768b6c088080a266ccc520b9cc6f96556ae
AAirCrafter <aaircrafter@gmail.com>
2025-07-09 14:21:20 +0200

    Delete src directory

--- src/main/java/air/dev/TimerSettings.java
+++
@@ -1,8 +0,0 @@
-package air.dev;
-
-public class TimerSettings {
-    public String prefix = "";
-    public String suffix = "";
-    public String defaultColor = "white";
-    public String finishedText = "{name} finished!";
-}
